aecee71f03c27b081f163f6d69cbbd75a832f715
moyuhaokan <1649189468@qq.com>
2020-04-29 23:51:41 +0800

    Create convertNumberToString.js

---
+++ week03/convertNumberToString.js
@@ -0,0 +1,16 @@
+
+function convertNumberToString(number, radix) {
+    let integer = Math.floor(number);
+    let fraction = String(number).match(/\.\d+$/);
+    if (fraction) {
+        fraction = fraction[0].replace('.', '');
+    }
+    let string = '';
+    while (integer > 0) {
+        string = String(integer % radix) + string;
+        integer = Math.floor(integer / radix);
+    }
+    return fraction ? `${string}.${fraction}` : string;
+}
+
+convertNumberToString(100, 10);
